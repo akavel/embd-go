b96b882f5116d11a9b39a3ea4e45da235fce3e80
Alexey Maslov <alexey.y.maslov@gmail.com>
2016-01-06 18:48:04 +0300

    added copyright.

--- embd.go
+++ embd.go
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2015 Mateusz Czapliński <czapkofan@gmail.com>
+Copyright (c) 2016 alxmsl <alexey.y.maslov@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
